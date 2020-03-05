f1619cd5ef00e5d1e15fd7ec6556838133bf9776
David Salgado <david@digitalronin.com>
2020-03-05 18:03:56 +0000

    Display a list of users

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
+const conn = new Pool({ connectionString: process.env.DATABASE_URL });
 
 express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => listUsers(req, res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+async function listUsers(req, res) {
+  try {
+    const db = await conn.connect()
+    const result = await db.query('SELECT * FROM users');
+    const results = { 'users': (result) ? result.rows : null};
+    res.render('pages/index', results );
+    db.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+}
