421fb50ca21bf425f1b2e09d4f8d99e9041ae17d
David Salgado <david@digitalronin.com>
2020-03-05 17:27:49 +0000

    Create a "Hello, World!" express app.
    This will be the starting point for a sample app.
    to accompany my "Top 5 ways to query your
    database in javascript" article."

---
+++ README.md
@@ -0,0 +1,5 @@
+# Top 5 ways to query your database using javascript
+
+This is the companion repository for this article.
+
+
---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
