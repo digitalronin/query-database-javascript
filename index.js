94486934313b83a84442d79cafb85a082fb616e7
David Salgado <david@digitalronin.com>
2020-03-11 21:00:20 +0000

    List users with mysql2 database library

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const mysql = require('mysql2/promise');
 
 express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => listUsers(req, res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+async function listUsers(req, res) {
+  try {
+    const conn = await mysql.createConnection(process.env.DATABASE_URL);
+    const [rows, fields] = await conn.execute('SELECT * FROM users');
+    const results = { 'users': rows };
+    res.render('pages/index', results );
+    await conn.end();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+}
